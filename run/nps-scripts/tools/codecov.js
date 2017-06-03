9d3a8dee6e230331549b12d9444e858f8e008dd6
Scott Steven Rodriguez <scott@BigGuy.movecodemove.com>
2017-06-02 22:12:46 -0400

    Codecov fix

--- run/nps-scripts/tools/codecov.js
+++ run/nps-scripts/tools/codecov.js
@@ -1,4 +1,4 @@
 module.exports = {
   description: 'Uploads coverage results to codecov',
-  script: 'codecov -t 85504922-8527-4454-a6c7-9b243fa05049'
+  script: 'codecov -t:85504922-8527-4454-a6c7-9b243fa05049'
 }
