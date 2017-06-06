de2936148466354bc096c29a33b5e493aea40945
Scott Steven Rodriguez <scott@BigGuy.movecodemove.com>
2017-06-06 08:17:54 -0400

    Handling codecov upload before build in deploy script

--- run/tasks/deploy.js
+++ run/tasks/deploy.js
@@ -3,8 +3,9 @@ const {log, shell: {rm, series}} = require('../utils/nps')
 const description = 'Verifies that tests pass and build completes succesfully and publishes to NPM'
 
 const nps = series(
-  'nps jest.coverage build',
+  'nps jest.coverage',
   'codecov',
+  'nps build',
   'npm publish'
 )
 
