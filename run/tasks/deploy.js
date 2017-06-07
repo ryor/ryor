be259ee40bb4534a39687a42681d0f3c96e81acb
Scott Steven Rodriguez <scott@BigGuy.movecodemove.com>
2017-06-07 05:29:44 -0400

    Deploy script fix

--- run/tasks/deploy.js
+++ run/tasks/deploy.js
@@ -1,12 +1,12 @@
-const {log, shell: {series}} = require('../utils/nps')
+const {log, shell: {cp, series}} = require('../utils/nps')
 
 const description = 'Verifies that tests pass and build completes succesfully and publishes to NPM'
 
 const nps = series(
   'echo',
-  log('Uploading code coverage results to CodeCov'),
+  log.task('Uploading code coverage results to CodeCov'),
   'codecov',
-  log('Publishing to NPM'),
+  log.task('Publishing to NPM'),
   cp('package.json', 'run/tools/npm/.npmrc', 'build'),
   'cd build',
   'npm publish',
