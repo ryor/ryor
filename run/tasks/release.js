0fec79cea4f8a766ad2ac68c3f9552f72303f04b
Scott Steven Rodriguez <scott@BigGuy.movecodemove.com>
2017-06-07 07:36:44 -0400

    Release task update

--- run/tasks/release.js
+++ run/tasks/release.js
@@ -1,4 +1,6 @@
 const {log, shell: {rm, series}} = require('../utils/nps')
+const semver = require('../../package.json').version.split('.')
+const version = `${semver[0]}.${semver[1]}.${Number(semver[2]) + 1}`
 
 const description = 'Verifies that tests pass and build completes succesfully, increments package.json patch numbers, commits changes to Git repository and pushes commit to Github'
 
@@ -18,8 +20,10 @@ const nps = series(
   log.task('Committing changes to Git repository'),
   'git add --all',
   'git commit',
-  log.task('Pushing changes to Github'),
-  'git push',
+  log.task('Adding new tag to Git repository'),
+  `git tag -a v${version} -m "Version ${version}"`,
+  log.task('Pushing commit and tag to Github'),
+  'git --follow-tags',
   'echo'
 )
 
