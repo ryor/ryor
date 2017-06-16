f21decf770179fb7ca7a89e24bddd73c150f5d78
Scott Steven Rodriguez <scott@movecodemove.com>
2017-06-16 14:04:05 -0400

    Cleaned up release task

--- run/tasks/release.js
+++ run/tasks/release.js
@@ -9,25 +9,19 @@ function run(args)
     throw new Error(`A commmit message is required to run the ${bold('release')} task`)
 
   return series(
-    log.task('Checking TypeScript for errors with TSLint'),
-    'tslint',
-    log.task('Testing TypeScript with Jest'),
-    'echo',
-    'jest',
-    'echo',
-    rm('coverage'),
-    log.task('Confirming build completes succesfully'),
-    'tsc',
-    'rollup',
-    rm('build'),
+    'test',
+    'build',
+    rm('build coverage'),
     log.task('Updating semver patch number in package.json file'),
     'patch',
     log.task('Committing changes to Git repository'),
-    `git commit '${args.join(' ')}'`,
+    `git commit '${args.join(' ')}'`
+    /*
     log.task('Adding new tag to Git repository'),
     'git tag',
     log.task('Pushing commit and tag to Github'),
     'git push'
+    */
   )
 }
 
