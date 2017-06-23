0aa8eaaafae12ea046b084d408e273f1428f1a47
Scott Steven Rodriguez <scott@movecodemove.com>
2017-06-23 13:12:51 -0400

    Updated commit task

--- run/tasks/commit.js
+++ run/tasks/commit.js
@@ -2,22 +2,55 @@ const description = 'Verifies that tests pass and build completes succesfully an
 
 function run(args)
 {
-  if (args.length === 0)
+  const minimist = require('minimist')
+  const {_, push, release} = minimist(args, {
+    alias: {p: 'push', r: 'release'},
+    boolean: ['p', 'push', 'r', 'release']
+  })
+
+  if (_.length === 0)
   {
     const {bold} = require('chalk')
 
     throw new Error(`A commmit message is required to run the ${bold('commit')} task`)
   }
 
-  return [
+  let series = [
     'test',
     'build',
-    'shx rm -rf build coverage',
+    'shx rm -rf build coverage'
+  ]
+
+  if (release)
+    series.push(
+      'log -w Updating semver patch numbers in package.json and README files',
+      'patch'
+    )
+
+  series.push(
     'log -w Committing changes to Git repository',
     'git add -A',
-    `git commit -m '${args.join(' ')}' --quiet`,
-    'log -s Changes committed'
-  ]
+    `git commit -m '${_.join(' ')}' --quiet`
+  )
+
+  if (release)
+  {
+    const currentVersion = require('../../package.json').version.split('.')
+    const newVersion = `${currentVersion[0]}.${currentVersion[1]}.${Number(currentVersion[2]) + 1}`
+
+    series.push(
+      'log -w Adding new tag to Git repository',
+      `git tag -a v${newVersion} -m "Version ${newVersion}"`
+    )
+  }
+
+  if (push || release)
+    series.push(
+      'log -w Pushing commit and tag to Github',
+      `git push${release ? ' --follow-tags' : ''}`
+    )
+
+  return series
 }
 
 module.exports = {description, run}
