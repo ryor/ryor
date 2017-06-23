4aff1852372c7d003e3eba4936156789e102539d
Scott Steven Rodriguez <scott@movecodemove.com>
2017-06-23 15:11:12 -0400

    Fixed publish task

--- run/tools/publish/index.js
+++ run/tools/publish/index.js
@@ -7,16 +7,15 @@ function run()
   const buildDirectoryPath = resolve(process.cwd(), 'build')
 
   if (!existsSync(buildDirectoryPath)
-  || !existsSync(resolve(buildDirectoryPath, 'index.js'))
-  || !existsSync(resolve(buildDirectoryPath, 'package.json')))
+  || !existsSync(resolve(buildDirectoryPath, 'index.js')))
   {
     const {bold} = require('chalk')
 
-    throw new Error(`Files required for npm publish were not found. Expected ${bold('build/index.js')} and ${bold('build/package.json')}.`)
+    throw new Error(`File required for npm publish was not found. Expected ${bold('build/index.js')}.`)
   }
 
   return [
-    'shx cp -rf README.md run/tools/publish/.npmrc build',
+    'shx cp -rf package.json README.md run/tools/publish/.npmrc build',
     'cd build',
     'npm publish'
   ]
