8efe68eea50182b6d3c46fbec3bd060e76385af2
Scott Steven Rodriguez <scott@movecodemove.com>
2017-06-27 14:18:12 -0400

    Moved Codecov upload to build in Travis config; pointing all README badges to master branch

--- run/tools/patch.js
+++ run/tools/patch.js
@@ -1,13 +1,11 @@
-const description = 'Increments semver patch numbers in the package.json and README files'
+const description = 'Increments semver patch numbers in package.json file'
 
 function run()
 {
   const {readFileSync, writeFileSync} = require('fs')
   const {resolve} = require('path')
   const packageJSONPath = resolve(__dirname, '../../package.json')
-  const readmePath = resolve(__dirname, '../../README.md')
   const packageJSON = require(packageJSONPath)
-  const readme = readFileSync(readmePath).toString()
   const currentVersion = packageJSON.version
   const currentVersionParts = currentVersion.split('.')
   const nextVersion = `${currentVersionParts[0]}.${currentVersionParts[1]}.${Number(currentVersionParts[2]) + 1}`
@@ -16,7 +14,6 @@ function run()
   packageJSON.devDependencies.ryor = currentVersion
 
   writeFileSync(packageJSONPath, JSON.stringify(packageJSON, null, 2))
-  writeFileSync(readmePath, readme.replace(new RegExp(currentVersion, 'g'), nextVersion))
 }
 
 module.exports = {description, run}
