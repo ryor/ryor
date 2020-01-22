f8620ec20c98ae0331eeafa75668cbe43b4e8941
Scott Steven Rodriguez <scott@movecodemove.com>
2020-01-22 06:48:05 +0100

    Updated commit runnable

--- run/tasks/commit.js
+++ run/tasks/commit.js
@@ -6,7 +6,7 @@ module.exports = {
     ['-b  --build',   'Commit changes only if build completes successfully'],
     ['-t  --test',    'Commit changes only if all tests pass'],
     ['-p  --push',    'Push commit'],
-    ['-r  --release', 'Increments package patch number and tags and pushes commit']
+    ['-r  --release', 'Pushes new tag after verifying tests pass and build completes succesfully']
   ]),
   run: args => {
     const { _, build, push, release, test } = require('minimist')(args, {
@@ -20,17 +20,17 @@ module.exports = {
 
     const sequence = []
 
-    if (test)
-      sequence.push(
-        'log -w Verifying that all tests pass',
-        'test -ps'
-      )
-
-    if (build)
+    if (build || release)
       sequence.push(
         'log -w Verifying that build completes successfully',
         'build -s',
         'shx rm -rf build'
+      )
+
+    if (test || release)
+      sequence.push(
+        'log -w Verifying that all tests pass',
+        'test -ps'
       )
 
     sequence.push(
