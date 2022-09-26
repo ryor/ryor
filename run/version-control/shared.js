16a8302dc785289ac0548782760497f8b8726c4a
Scott Steven Rodriguez <scott@movecodemove.com>
2022-09-26 09:40:01 +0100

    Removed git runnable Added release option to branch runnable Updated version control utility functions

--- run/version-control/shared.js
+++ run/version-control/shared.js
@@ -1,78 +1,74 @@
-async function getCommandOutput(commandString) {
+async function runShellCommand(command) {
   const { spawn } = await import('child_process')
 
   return new Promise((resolve) => {
-    const [command, ...args] = commandString.split(' ')
-    const childProcess = spawn(command, args)
-    let output = ''
+    const [name, ...args] = command.split(' ')
+    const childProcess = spawn(name, args)
+    let stderr = ''
+    let stdout = ''
 
-    childProcess.stdout.on('data', (data) => {
-      output += data.toString()
+    childProcess.stderr.on('data', (data) => {
+      stderr += data.toString()
     })
-
-    childProcess.on('close', () => resolve(output.trim()))
+    childProcess.stdout.on('data', (data) => {
+      stdout += data.toString()
+    })
+    childProcess.on('close', (code) => resolve({ code, stderr: stderr.trim(), stdout: stdout.trim() }))
   })
 }
 
+export async function doesTagExist(name) {
+  return (await runShellCommand(`git tag -v ${name}`)).stderr !== `error: tag '${name}' not found.`
+}
+
 export async function getAllBranches() {
-  const { spawn } = await import('child_process')
+  const { stdout } = await runShellCommand('git branch --all')
 
-  return new Promise((resolve) =>
-    spawn('git', ['branch', '--all']).stdout.on('data', (data) => {
-      resolve(
-        data
-          .toString()
-          .split('\n')
-          .map((line) => line.trim())
-          .filter((line) => !!line)
-          .reduce(
-            (result, line) => {
-              if (line.startsWith('*')) {
-                const branchName = line.split('*')[1].trim()
+  return stdout
+    .split('\n')
+    .map((line) => line.trim())
+    .filter((line) => !!line)
+    .reduce(
+      (result, line) => {
+        if (line.startsWith('*')) {
+          const branchName = line.split('*')[1].trim()
 
-                return {
-                  ...result,
-                  current: branchName,
-                  local: [...result.local, branchName]
-                }
-              } else if (line.startsWith('remotes/origin/')) {
-                if (!line.includes('HEAD')) {
-                  return {
-                    ...result,
-                    remote: [...result.remote, line.split('remotes/origin/')[1]]
-                  }
-                }
+          return {
+            ...result,
+            current: branchName,
+            local: [...result.local, branchName]
+          }
+        } else if (line.startsWith('remotes/origin/')) {
+          if (!line.includes('HEAD')) {
+            return {
+              ...result,
+              remote: [...result.remote, line.split('remotes/origin/')[1]]
+            }
+          }
 
-                return result
-              } else return { ...result, local: [...result.local, line] }
-            },
-            { current: null, local: [], remote: [] }
-          )
-      )
-    })
-  )
+          return result
+        } else return { ...result, local: [...result.local, line] }
+      },
+      { current: null, local: [], remote: [] }
+    )
 }
 
 export async function getCurrentBranch() {
-  return getCommandOutput('git branch --show-current')
+  return (await runShellCommand('git branch --show-current')).stdout
 }
 
 export async function isCommitRequired() {
-  return (await getCommandOutput('git status -s')) !== ''
+  return (await runShellCommand('git status -s')).stdout !== ''
 }
 
 export async function isExistingBranch(name) {
-  const { spawn } = await import('child_process')
-
-  return new Promise((resolve) => spawn('git', ['rev-parse', '--verify', `"${name}"`]).on('close', (code) => resolve(code === 0)))
+  return (await runShellCommand(`git rev-parse --verify ${name}`)).code === 0
 }
 
 export async function isPushRequired() {
-  return (await getCommandOutput('git status')).includes('Your branch is ahead of')
+  return (await runShellCommand('git status')).stdout.includes('Your branch is ahead of')
 }
 
 export async function isValidBranchName(name) {
-  const { spawn } = await import('child_process')
-
-  return new Promise((resolve) => spawn('git', ['check-ref-format', '--branch', `"${name}"`]).on('close', (code) => resolve(code === 0)))
+  return (await runShellCommand(`git check-ref-format --branch ${name}`)).code === 0
 }
