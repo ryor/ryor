a3e6456254ad4bc52c63512f7d6bbb86aa24c94c
Scott Steven Rodriguez <scott@movecodemove.com>
2023-11-01 18:44:59 -0400

    Release 0.9.19

--- tasks/version-control/branch.js
+++ tasks/version-control/branch.js
@@ -79,8 +79,6 @@ export async function run({ _, ...args }) {
     if (local.includes(releaseBranch)) return [`git checkout ${releaseBranch}`, 'git branch --all']
     else if (remote.includes(releaseBranch)) return [`git checkout ${releaseBranch}`, 'git pull', 'git branch --all']
 
-    packageJSON.version = releaseVersion
-
     await writeFile('package.json', JSON.stringify(packageJSON, null, '  '))
 
     return [
